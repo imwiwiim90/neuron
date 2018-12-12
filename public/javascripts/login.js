5013fe91acda333ab2deab10492a6e6a69d0750e
imwiwiim90 <imwiwiim90@gmail.com>
2018-12-12 10:45:50 -0500

    some hanges

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,6 +1,7 @@
 $(document).ready(() => {
 
-$('#btn-submit').on('click', () => {
+$('#btn-submit').on('click', (e) => {
+	e.preventDefault();
 	let data = {
 		user: $('#email-input').val(),
 		pass: $('#pass-input').val(),
@@ -12,8 +13,14 @@ $('#btn-submit').on('click', () => {
 	}).done((response) => {
 		if (response == 'true') {
 			window.location.href = '/views/dashboard.html';
-		}
+		} else err_msg('username or password is invalid');
 	});
-});	
+});
+
+function err_msg(txt) {
+	$(".invalid-feedback").show();
+	$('.invalid-feedback').html(txt);
+} 	
+
 
 });	
