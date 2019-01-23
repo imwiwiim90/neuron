8b114b306d4a1a83a9b72d6d155f08d7d6221c4d
imwiwiim90 <imwiwiim90@gmail.com>
2019-01-23 14:37:26 -0500

    redirection from /

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/views/index.html');
 });
 
 router.post('/signup', function(req, res, next) {
