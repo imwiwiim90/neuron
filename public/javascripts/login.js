fa5053ca1b59723b0bd94b3085ad6e515a1cb567
imwiwiim90 <imwiwiim90@gmail.com>
2019-02-01 16:23:09 -0500

    adding message when wrong credentials;

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -10,13 +10,23 @@ $('#btn-submit').on('click', (e) => {
 		url: '/users/login',
 		method: 'POST',
 		data: data,
+		error: (req,status,error) => {
+			err_msg("Usuario o contraseña invalidos");
+		}
 	}).done((response) => {
 		if (response != 'false') {
 			localStorage.setItem('user_id', response);
 			window.location.href = '/views/dashboard.html';
 		} else err_msg('username or password is invalid');
-
 	});
+});
+
+$('#email-input').on('input',() => {
+	$('.invalid-feedback').hide();
+});
+
+$('#pass-input').on('input',() => {
+	$('.invalid-feedback').hide();
 });
 
 function err_msg(txt) {
